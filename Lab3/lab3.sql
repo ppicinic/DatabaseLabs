7977a69c1e8b2cf16d4a51f0a9f5a30c17ee340f
Phil Picinic <philpicinic@gmail.com>
2015-02-23 01:41:40 -0500

    lab 3 and 4 add on 2-23-15

---
+++ Lab3/lab3.sql
@@ -0,0 +1,47 @@
+﻿-- 1)
+SELECT ordno, dollars 
+FROM orders;
+
+-- 2)
+SELECT name, city 
+FROM agents 
+WHERE name='Smith';
+ 
+-- 3)
+SELECT pid, name, priceusd 
+FROM products 
+WHERE quantity>200000;
+
+-- 4)
+SELECT name, city 
+FROM customers;
+
+-- 5)
+SELECT name, city 
+FROM agents 
+WHERE city!='New York' 
+	AND city!='Tokyo';
+
+-- 6)
+SELECT * 
+FROM products 
+WHERE city!='Dallas' 
+	AND city!='Duluth' 
+	AND priceusd>=1;
+
+-- 7)
+SELECT * 
+FROM orders 
+WHERE mon='jan' 
+	OR mon='may';
+
+-- 8)
+SELECT * 
+FROM orders 
+WHERE mon='feb' 
+	AND dollars>500;
+
+-- 9)
+SELECT * 
+FROM orders 
+WHERE cid='c005';
---
+++ Lab4/lab4.sql
@@ -0,0 +1,69 @@
+﻿-- 1)
+SELECT city
+
+FROM agents
+WHERE aid IN (
+SELECT aid
+FROM orders
+WHERE cid = 'c006');
+
+-- 2)
+
+SELECT DISTINCT pid
+FROM orders
+WHERE cid =(
+	SELECT cid
+	FROM customers
+	WHERE city = 'Kyoto')
+ORDER BY pid DESC;
+
+-- 3)
+
+SELECT DISTINCT cid, name
+FROM customers
+WHERE cid NOT IN (
+		SELECT cid
+		FROM orders
+		WHERE aid = 'a03');
+
+-- 4)
+
+SELECT DISTINCT cid
+FROM customers
+WHERE cid NOT IN (
+		SELECT cid
+		FROM orders
+		WHERE pid != 'p01' 
+			AND pid != 'p07');
+
+-- 5)
+SELECT DISTINCT pid
+FROM products
+WHERE pid NOT IN ( 
+	SELECT DISTINCT pid
+	FROM orders
+	WHERE cid NOT IN (
+			SELECT cid
+			FROM orders
+			WHERE aid = 'a05'));
+
+-- 6)
+SELECT name, discount, city
+FROM customers
+WHERE cid IN (
+		SELECT DISTINCT cid
+		FROM orders
+		WHERE aid IN (
+				SELECT aid
+				FROM agents
+				WHERE city = 'Dallas'
+					OR city = 'New York'));
+
+-- 7)
+SELECT *
+FROM customers
+WHERE discount IN (
+			SELECT discount
+			FROM customers
+			WHERE city = 'Dallas'
+				OR city = 'London');
